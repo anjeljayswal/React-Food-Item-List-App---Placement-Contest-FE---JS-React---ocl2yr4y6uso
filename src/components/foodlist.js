e3b1a8577b1d21c6f8f9d69c7765eb39cfca9728
anjeljayswal <53367474+anjeljayswal@users.noreply.github.com>
2023-08-27 19:02:59 +0545

    Update foodlist.js

--- src/components/foodlist.js
+++ src/components/foodlist.js
@@ -8,37 +8,53 @@ function FoodList() {
 	const [spicinessLevel, setSpicinessLevel] = useState("");
 	const [isFirstCardEnabled, setIsFirstCardEnabled] = useState(false);
 	const [isSecondCardEnabled, setIsSecondCardEnabled] = useState(false);
-	const [isFormEnabled, setIsFormEnabled] = useState(false);
+	const [isFormEnabled, setIsFormEnabled] = useState(false); 
+
+	const handleAddFood = () =>{
+		const newFood = {
+			itemName, foodType, SpicinessLevel,
+		};
+		setFoods([...foods, newFood]);
+
+		setItemName("");
+		setFoodType("");
+		setSpicinessLevel("");
+	};
 
 	return (
 		<>
 			<div className="container">
 				<h1>Food Items List</h1>
-				<button>Add Food</button>
+				<button onClick={() => setIsFirstCardEnabled(true)}>Add Food</button>
 
 				<div className="card-container">
+	{isFirstCardEnabled && (
                         <>
 							<h2>Item Name:</h2>
 							<input
 								name="itemName"
 								type="text"
+								value={itemName}
 								disabled={!isFirstCardEnabled}
 							/>
 							<h2>Food Type:</h2>
 							<input
 								name="foodType"
 								type="text"
+								value={foodType}
 								disabled={!isFirstCardEnabled}
 							/>
-							<div className={`card`}>
+							<div className={`card ${isFormEnabled ? "" : "disabled"}` }>
 								<form>
 									<h2>Spiciness Level:</h2>
 									<input
 										name="spicinessLevel"
 										type="text"
+										value={spicinessLevel}
 										disabled={!isFormEnabled}
 									/>
 								</form>
+								<button onClick={handleAddFood}>Save</button>
 							</div>
 						</>
 				</div>
